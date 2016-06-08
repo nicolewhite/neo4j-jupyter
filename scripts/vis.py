2ca1abc049474b76e1caec9aa3c87fd5261a4ced
Nicole White <nicole.m.white@outlook.com>
2016-06-08 11:43:43 -0700

    Update draw function for py2neo v3

--- scripts/vis.py
+++ scripts/vis.py
@@ -18,12 +18,12 @@ def vis_network(nodes, edges, physics=False):
       var edges = {edges};
 
       var container = document.getElementById("{id}");
-      
+
       var data = {{
         nodes: nodes,
         edges: edges
       }};
-      
+
       var options = {{
           nodes: {{
               shape: 'dot',
@@ -47,7 +47,7 @@ def vis_network(nodes, edges, physics=False):
               enabled: {physics}
           }}
       }};
-      
+
       var network = new vis.Network(container, data, options);
 
     </script>
@@ -57,7 +57,7 @@ def vis_network(nodes, edges, physics=False):
 
     unique_id = str(uuid.uuid4())
     html = html.format(id=unique_id, nodes=json.dumps(nodes), edges=json.dumps(edges), physics=json.dumps(physics))
-    
+
     filename = "figure/graph-{}.html".format(unique_id)
 
     file = open(filename, "w")
@@ -67,55 +67,53 @@ def vis_network(nodes, edges, physics=False):
     return IFrame(filename, width="100%", height="400")
 
 def draw(graph, options, physics=False, limit=100):
-    # The options argument should be a dictionary of node labels and property keys; it determines which property 
+    # The options argument should be a dictionary of node labels and property keys; it determines which property
     # is displayed for the node label. For example, in the movie graph, options = {"Movie": "title", "Person": "name"}.
     # Omitting a node label from the options dict will leave the node unlabeled in the visualization.
     # Setting physics = True makes the nodes bounce around when you touch them!
     query = """
-    MATCH n
-    WITH n, RAND() AS random
-    ORDER BY random LIMIT {limit}
+    MATCH (n)
+    WITH n, rand() AS random
+    ORDER BY random
+    LIMIT {limit}
     OPTIONAL MATCH (n)-[r]->(m)
-    RETURN n, r, m
+    RETURN n AS source_node,
+           id(n) AS source_id,
+           r,
+           m AS target_node,
+           id(m) AS target_id
     """
 
-    data = graph.cypher.execute(query, limit=limit)
+    data = graph.run(query, limit=limit)
 
     nodes = []
     edges = []
 
-    def get_vis_info(node):
-        node_label = list(node.labels)[0]
+    def get_vis_info(node, id):
+        node_label = list(node.labels())[0]
         prop_key = options.get(node_label)
         vis_label = node.properties.get(prop_key, "")
-        vis_id = node.ref.split("/")[1]
 
-        title = {}
-
-        for key, value in node.properties.items():
-            key = key.encode("utf8")
-            value = value.encode("utf8") if type(value) is unicode else value
-
-            title[key] = value
-
-        return {"id": vis_id, "label": vis_label, "group": node_label, "title": repr(title)}
+        return {"id": id, "label": vis_label, "group": node_label, "title": repr(node.properties)}
 
     for row in data:
-        source = row[0]
-        rel = row[1]
-        target = row[2]
+        source_node = row[0]
+        source_id = row[1]
+        rel = row[2]
+        target_node = row[3]
+        target_id = row[4]
 
-        source_info = get_vis_info(source)
+        source_info = get_vis_info(source_node, source_id)
 
         if source_info not in nodes:
             nodes.append(source_info)
 
-        if rel:
-            target_info = get_vis_info(target)
+        if rel is not None:
+            target_info = get_vis_info(target_node, target_id)
 
             if target_info not in nodes:
                 nodes.append(target_info)
 
-            edges.append({"from": source_info["id"], "to": target_info["id"], "label": rel.type})
+            edges.append({"from": source_info["id"], "to": target_info["id"], "label": rel.type()})
 
     return vis_network(nodes, edges, physics=physics)
